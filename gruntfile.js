63ed0fcd28691a24f3e4264e10152c8976dfcf9e
Marlon E <icecold29@gmail.com>
2016-04-01 15:56:39 -0700

    gruntfile cleanup

--- gruntfile.js
+++ gruntfile.js
@@ -2,48 +2,29 @@ module.exports = function(grunt) {
     var buildFiles = ['gruntfile.js', 'build/**/*.js', 'config/*.js', 'server.js'];
 
     var jsWeb = [
-        //gensler
-
-        // tools
+        // all js files of tools need to be listed here. order matters.
         'public/tool/mobiletool/mobiletool.js'
-        
-        /*'public/tool/left-panel/left-panel.js',
-
-        'public/tool/story-card/story-card.js',
-
-        'public/tool/task-card/task-card.js',
-
-        // app is always last
-        'public/js/app/app.js'*/
     ];
 
     var cssWeb = [
-        // tools
+        // all css files of tools need to be listed here. order matters.
         'public/tool/mobiletool/mobiletool.css',
-       /* 'public/tool/task-card/task-card.css',
-
-        // app is always last
-        'public/css/app.css'*/
     ];
 
-    grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-jsbeautifier');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');    
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
+    // our custom grunt tasks
     grunt.loadTasks('build/tasks');
 
     // Project configuration.
     grunt.initConfig({
         pkg: '<json:package.json>',
         banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>\n' + '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' + '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' + '*/\n',
-        jsbeautifier: {
-            web: jsWeb,
-            grunt: buildFiles
-        },
         jshint: {
             options: {
                 jshintrc: '.jshintrc'
@@ -87,14 +68,11 @@ module.exports = function(grunt) {
         }
     });
 
-    // JS Beautify.
-    grunt.registerTask('beautify', ['jsbeautifier']);
     // Production task.
     grunt.registerTask('prod', ['clean', 'concat', 'cssmin', 'uglify', 'imagerustler']);
     // Deploy task.
     grunt.registerTask('deploy', ['clean', 'concat', 'cssmin', 'uglify', 'imagerustler']);
     // Dev mode: no minifying.
-    //grunt.registerTask('default', 'clean lint beautify csslint qunit concat');
-    grunt.registerTask('default', ['clean', 'jshint', 'beautify', 'csslint', 'concat', 'imagerustler']);
+    grunt.registerTask('default', ['clean', 'jshint', 'csslint', 'concat', 'imagerustler']);
 
 };
